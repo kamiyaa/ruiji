c4d7bc26049f46a3950462402016bce36538cb57
Jiayi Zhao <jeff.no.zhao@gmail.com>
2016-12-31 19:51:51 -0500

    clean up config.mk

--- src/config.mk
+++ src/config.mk
@@ -1,20 +1,23 @@
 # compiler and linker
 CC = clang
 
+# Comment to remove color output support
 COLORFLAG = -DCOLOR
 
+# All optional features
 USE_FLAGS = ${COLORFLAG}
 
-# flags
+# cflags
 CFLAGS = -std=gnu99 -ggdb3 -O0 -Wall -Werror -Wno-unused-variable
 
+# libraries required
 LIBS = -lcurl
 
+# helper files for specific websites
 WEBSRC = danbooru.c eshuushuu.c gelbooru.c konachan.c sankakucomplex.c yandere.c zerochan.c
+# Helper files to for main program
 HELPERS = $(WEBSRC) interface.c parser.c udload.c
 
-# LIBS = -lncurses `pkg-config --cflags --libs gtk+-3.0`
-
+# paths
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
-
