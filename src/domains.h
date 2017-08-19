de26c456ae1520769642cfa5cb421da751d74768
Jiayi Zhao <jeff.no.zhao@gmail.com>
2017-08-18 20:29:26 -0400

    add unique ids to determine what website image is from

--- src/domains.h
+++ src/domains.h
@@ -7,11 +7,19 @@
 #include "yandere.h"
 #include "zerochan.h"
 
-#define DANBOORU_DOMAIN "danbooru.donmai.us"
-#define ESHUUSHUU_DOMAIN "e-shuushuu.net"
-#define GELBOORU_DOMAIN "gelbooru.com"
-#define KONACHAN_DOMAIN "konachan.com"
-#define MANGADRAWING_DOMAIN "mangadrawing.net"
-#define SANKAKU_COMPLEX_DOMAIN "chan.sankakucomplex.com"
-#define YANDERE_DOMAIN "yande.re"
-#define ZEROCHAN_DOMAIN "www.zerochan.net"
+#define DANBOORU_DOMAIN		"danbooru.donmai.us"
+#define ESHUUSHUU_DOMAIN	"e-shuushuu.net"
+#define GELBOORU_DOMAIN		"gelbooru.com"
+#define KONACHAN_DOMAIN		"konachan.com"
+#define MANGADRAWING_DOMAIN	"mangadrawing.net"
+#define SANKAKUCOMPLEX_DOMAIN	"chan.sankakucomplex.com"
+#define YANDERE_DOMAIN		"yande.re"
+#define ZEROCHAN_DOMAIN		"www.zerochan.net"
+
+#define YANDERE_UUID		1
+#define DANBOORU_UUID		2
+#define SANKAKUCOMPLEX_UUID	3
+#define KONACHAN_UUID		4
+#define ESHUUSHUU_UUID		5
+#define GELBOORU_UUID		6
+#define MANGADRAWING_UUID	7
