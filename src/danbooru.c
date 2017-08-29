85b0940e80c0011475ac14c009fbe5d3479c5f65
Jiayi Zhao <jeff.no.zhao@gmail.com>
2017-08-29 09:08:34 -0400

    danbooru.c: remove old code that relied on parsing html rather than json

--- src/danbooru.c
+++ src/danbooru.c
@@ -20,50 +20,6 @@ char *danbooru_generate_api_url(char *url)
 	return api_url;
 }
 
-char *danbooru_get_image_url(char *html_content)
-{
-	/* constants used to find values */
-	const char danbooru_url[] = "https://danbooru.donmai.us";
-
-	const char source_uuid[] = "Size: <a href=\"";
-	const char source_end = '"';
-
-	const unsigned int source_uuid_len = strlen(source_uuid);
-	const unsigned int danbooru_url_len = strlen(danbooru_url);
-
-	/* initialize the image source url to be returned later */
-	char *img_src_url = NULL;
-
-	/* Find the source image link */
-	char *source_index = strstr(html_content, source_uuid);
-
-	/* If source image link is found,
-	 * add the danbooru url to it and return it */
-	if (source_index) {
-		/* move source_index pointer to the beginning of
-		 * the source image url */
-		source_index = &(source_index[source_uuid_len]);
-		/* get the length of the source image url */
-		int url_len = get_distance(source_index, source_end);
-
-		/* allocate enough memory to hold the image source url,
-		 * then copy the url over to img_src_url and return it */
-		img_src_url = malloc(CHAR_SIZE *
-					(url_len + danbooru_url_len + 1));
-
-		strncpy(img_src_url, danbooru_url, danbooru_url_len);
-		strncpy(&(img_src_url[danbooru_url_len]),
-			source_index, url_len + 1);
-	}
-	else {
-		fprintf(stderr,
-			"danbooru_get_image_url(): Error: Failed to parse website\n");
-	}
-
-	/* return the image source url */
-	return img_src_url;
-}
-
 char *danbooru_get_image_url_json(char *json_content)
 {
 
@@ -105,80 +61,6 @@ char *danbooru_get_image_url_json(char *json_content)
 	return img_src_url;
 }
 
-struct image_tag_db *danbooru_get_image_tags(char *html_content)
-{
-	/* constants for finding values */
-	const char tags_uuid[] = "<section id=\"tag-list\">";
-	const char tags_end[] = "</section>";
-	const char tag_category_uuid[] = "category-";
-	const char tag_name_uuid[] = "href=\"/posts?tags=";
-	const char tag_name_end = '"';
-
-	const unsigned int initial_offset = strlen(tags_uuid);
-	const unsigned int category_offset = strlen(tag_category_uuid);
-	const unsigned int name_offset = strlen(tag_name_uuid);
-
-	/* set tag_ptr to the beginning in which the tags begin */
-	char *tag_contents = strstr(html_content, tags_uuid);
-	if (tag_contents) {
-		tag_contents = &(tag_contents[initial_offset]);
-		char *tag_contents_end = strstr(tag_contents, tags_end);
-		tag_contents_end[0] = '\0';
-	}
-
-	/* initialize a tags database to store tags */
-	struct image_tag_db *tag_db = init_image_tag_db();
-	struct llnode **tag_ptrs[6] = {
-		&(tag_db->tags[0]),
-		&(tag_db->tags[1]),
-		&(tag_db->tags[2]),
-		&(tag_db->tags[3]),
-		&(tag_db->tags[4]),
-		&(tag_db->tags[5])
-	};
-
-	/* set pointer to beginning of first tag */
-	tag_contents = strstr(tag_contents, tag_category_uuid);
-	while (tag_contents) {
-		/* move to beginning of tag type */
-		tag_contents = &(tag_contents[category_offset]);
-		/* get the tag type of the tag */
-		unsigned int tag_index = danbooru_get_tag_type(tag_contents[0]);
-
-		/* move pointer to the start of the tag name */
-		tag_contents = strstr(tag_contents, tag_name_uuid);
-		tag_contents = &(tag_contents[name_offset]);
-
-		/* get where the tag name ends is */
-		int tag_name_len = get_distance(tag_contents, tag_name_end);
-
-		/* allocate enough memory for the tag name + null terminator */
-		char *tag_name = malloc(CHAR_SIZE * (tag_name_len + 1));
-		strncpy(tag_name, tag_contents, tag_name_len);
-		tag_name[tag_name_len] = '\0';
-
-
-		/* allocate memory for node */
-		*(tag_ptrs[tag_index]) = malloc(LLNODE_SIZE);
-
-		(*(tag_ptrs[tag_index]))->next = NULL;
-		(*(tag_ptrs[tag_index]))->data = tag_name;
-
-		/* set tag_ptrs to its next value */
-		tag_ptrs[tag_index] = &((*(tag_ptrs[tag_index]))->next);
-
-
-		/* increment the amount of tags in this category we currently
-		 * found */
-		(tag_db->tag_size[tag_index])++;
-
-		tag_contents = &(tag_contents[tag_name_len]);
-		tag_contents = strstr(tag_contents, tag_category_uuid);
-	}
-
-	return tag_db;
-}
-
 struct image_tag_db *danbooru_get_image_tags_json(char *json_content)
 {
 	char artist_tag_uuid[] = "\"tag_string_artist\":\"";
@@ -189,14 +71,29 @@ struct image_tag_db *danbooru_get_image_tags_json(char *json_content)
 	/* initialize a tags database to store tags */
 	struct image_tag_db *tag_db = init_image_tag_db();
 
-	tag_db->tags[0] = danbooru_parse_tags_json(artist_tag_uuid, json_content,
-				&(tag_db->tag_size[0]));
-	tag_db->tags[1] = danbooru_parse_tags_json(character_tag_uuid, json_content,
-				&(tag_db->tag_size[1]));
-	tag_db->tags[3] = danbooru_parse_tags_json(copyright_tag_uuid, json_content,
-				&(tag_db->tag_size[3]));
-	tag_db->tags[5] = danbooru_parse_tags_json(general_tag_uuid, json_content,
-				&(tag_db->tag_size[5]));
+	tag_db->tags[0] = danbooru_parse_tags_json(
+				artist_tag_uuid,
+				json_content,
+				&(tag_db->tag_size[0])
+				);
+
+	tag_db->tags[1] = danbooru_parse_tags_json(
+				character_tag_uuid,
+				json_content,
+				&(tag_db->tag_size[1])
+				);
+
+	tag_db->tags[3] = danbooru_parse_tags_json(
+				copyright_tag_uuid,
+				json_content,
+				&(tag_db->tag_size[3])
+				);
+
+	tag_db->tags[5] = danbooru_parse_tags_json(
+				general_tag_uuid,
+				json_content,
+				&(tag_db->tag_size[5])
+				);
 
 	return tag_db;
 }
@@ -260,30 +157,4 @@ struct llnode *danbooru_parse_tags_json(char *tag_pattern, char *json_content,
 		*ptr_end = ',';
 	}
 	return tags;
-}
-
-unsigned int danbooru_get_tag_type(char category)
-{
-	unsigned int tag_index;
-	/* figure out what tag type this is */
-	switch (category) {
-	/* artist */
-	case '1':
-		tag_index = 0;
-		break;
-	/* copyright */
-	case '3':
-		tag_index = 3;
-		break;
-	/* character */
-	case '4':
-		tag_index = 1;
-		break;
-	/* general */
-	case '0':
-	default:
-		tag_index = 5;
-		break;
-	}
-	return tag_index;
 }
--- src/danbooru.h
+++ src/danbooru.h
@@ -4,20 +4,11 @@
  */
 char *danbooru_generate_api_url(char *url);
 
-/* Given the html of a danbooru post webpage, parse the html
- * to get the source image url
- */
-char *danbooru_get_image_url(char *html_content);
-
 /* Given the json of a danbooru post webpage, parse the json
  * to get the source image url
  */
 char *danbooru_get_image_url_json(char *json_content);
 
-/* Given the html of a danbooru post webpage, parse the html
- * to get the tags of the image
- */
-struct image_tag_db *danbooru_get_image_tags(char *html_content);
 
 /* Given the json of a danbooru post webpage, parse the json
  * to get the tags of the image
@@ -26,5 +17,3 @@ struct image_tag_db *danbooru_get_image_tags_json(char *json_content);
 
 struct llnode *danbooru_parse_tags_json(char *tag_pattern, char *json_content,
 	unsigned int *size);
-
-unsigned int danbooru_get_tag_type(char category);
